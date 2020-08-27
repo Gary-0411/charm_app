293cbba2c61b322c0d6faf52af2c5169a433c5f9
SDX <282919162@qq.com>
2020-08-27 17:30:46 +0800

    cimit

--- lib/main.dart
+++ lib/main.dart
@@ -10,43 +10,25 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-//        colorScheme: ColorScheme(
-//          primary: Colors.cyan[100],
-//          primaryVariant: Colors.cyan[200],
-//          secondary: Colors.cyan[300],
-//          secondaryVariant: Colors.cyan[400],
-//          surface: Colors.cyan[500],
-//          background: Colors.cyan[600],
-//          error: Colors.cyan[700],
-//          onPrimary: Colors.cyan[800],
-//          onSecondary: Colors.cyan[900],
-//          onSurface: Colors.cyanAccent[100],
-//          onBackground: Colors.cyanAccent[200],
-//          onError: Colors.cyanAccent[400],
-//          brightness: Brightness.dark,
-//        ),
-//        primarySwatch: Colors.cyan,
-//        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      theme: ThemeData(),
       home: MaterialApp(
         theme: ThemeData(),
         home: Scaffold(
-          appBar: AppBar(),
+//          appBar: AppBar(title: Text("XXX")),
           body: MainPage(),
           bottomNavigationBar: BottomNavigationBar(
-//            backgroundColor: Colors.cyan[500],
-//            selectedItemColor: Colors.white,
-//            showUnselectedLabels: true,
-//            unselectedItemColor: Colors.black,
             items: [
               BottomNavigationBarItem(
-                icon: Icon(Icons.ac_unit),
+                icon: Icon(Icons.view_quilt),
                 label: "干货",
               ),
               BottomNavigationBarItem(
-                icon: Icon(Icons.ac_unit),
+                icon: Icon(Icons.android),
                 label: "玩安卓",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.account_box),
+                label: "个人中心",
               ),
             ],
           ),
