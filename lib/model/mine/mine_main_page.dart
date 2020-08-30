536f969fe0742dd09439e0bee362ff18034763a1
Gary <soudx@foxmail.com>
2020-08-30 22:37:45 +0800

    commit

--- lib/model/mine/mine_main_page.dart
+++ lib/model/mine/mine_main_page.dart
@@ -18,16 +18,11 @@ class MineMainState extends State<MineMainPage> {
   void initState() {
     super.initState();
     _scrollController.addListener(() {
-      // setState(() {
-      // a = 1 - _scrollController.offset / 472.0.w;
-      // });
-      // print("a = ${a}");
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    print("100.w = " + 100.w.toString());
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
@@ -37,64 +32,83 @@ class MineMainState extends State<MineMainPage> {
           ],
         ),
       ),
-      child: NestedScrollView(
-//        floatHeaderSlivers: true,
-        controller: _scrollController,
-        headerSliverBuilder: (context, innerBoxIsScrolled) {
-          print("context.size = ${context.widget}");
-          return <Widget>[
-            SliverAppBar(
-              shadowColor: Colors.transparent,
-              backgroundColor: Color(0xFFfcd0bc),
-              // backgroundColor: Colors.transparent,
-              expandedHeight: 472.0.w,
-              floating: true,
-              stretch: true,
-              onStretchTrigger: () {
-                print("onStretchTrigger");
-                return;
-              },
-              pinned: true,
-              flexibleSpace: FlexibleSpaceBar(
-                collapseMode: CollapseMode.pin,
-                background: Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Color(0xFFfcd0bc),
-                        Color(0xFFf4b3b1),
-                      ],
-                    ),
-                  ),
-                ),
-                centerTitle: false,
-                stretchModes: [StretchMode.fadeTitle],
-                title: Text(
-                  "XXXX",
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
-            ),
-          ];
+      child: NotificationListener(
+        onNotification: (notification) {
+          if (notification is ScrollUpdateNotification) {
+          } else if (notification is ScrollStartNotification) {
+          } else if (notification is ScrollEndNotification) {
+            print("notification = ${notification.metrics.extentBefore}");
+            print("notification = ${notification.metrics.atEdge}");
+          } else if (notification is UserScrollNotification) {
+          } else if (notification is OverscrollNotification) {
+          } else {
+            print("notification = $notification");
+          }
+          return;
         },
-        body: Container(
-          child: Center(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(kToolbarHeight),
-                    topRight: Radius.circular(kToolbarHeight)),
-              ),
-              child: ListView.builder(
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    leading: Icon(Icons.android),
-                    title: Text('Gay里Gay气的标题+$index'),
-                  );
+        child: NestedScrollView(
+          controller: _scrollController,
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                shadowColor: Colors.transparent,
+                backgroundColor: Color(0xFFfcd0bc),
+                // backgroundColor: Colors.transparent,
+                expandedHeight: 472.0.w,
+                floating: false,
+                stretch: true,
+                snap: false,
+                onStretchTrigger: () {
+                  print("onStretchTrigger");
+                  return;
                 },
-                itemCount: 35,
+                pinned: true,
+                flexibleSpace: LayoutBuilder(builder: (context, snapshot) {
+                  double percent = ((snapshot.maxHeight - kToolbarHeight) /
+                      (472.0.w - kToolbarHeight));
+                  double percenta = 1 - percent;
+                  return FlexibleSpaceBar(
+                    collapseMode: CollapseMode.none,
+                    background: Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Color(0xFFfcd0bc),
+                            Color(0xFFf4b3b1),
+                          ],
+                        ),
+                      ),
+                    ),
+                    centerTitle: false,
+                    stretchModes: [StretchMode.fadeTitle],
+                    titlePadding: EdgeInsets.only(left: 120.w,bottom: 300.w * percent+16.w),
+                    title: Visibility(
+                      visible: true,
+                      child: Text(
+                        "XXXX",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  );
+                }),
               ),
+            ];
+          },
+          body: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(kToolbarHeight * a),
+                  topRight: Radius.circular(kToolbarHeight * a)),
+            ),
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Icon(Icons.android),
+                  title: Text('Gay里Gay气的标题+$index'),
+                );
+              },
+              itemCount: 35,
             ),
           ),
         ),
